9193cc921efa594849dfd147191b8995b0c7ac4d
Gabriel Bertaglia <gaabrielcursi@gmail.com>
2026-02-01 11:42:44 -0300

    fix: vote_average

--- src/components/rating/index.tsx
+++ src/components/rating/index.tsx
@@ -8,7 +8,7 @@ export function Rating({ vote_average }: { vote_average: number }) {
 		<View style={styles.rating}>
 			<Star size={14} color="#FFC319" fill="#FFC319" />
 			<Text size={12} style={{ color: '#9C9C9C' }}>
-				{(vote_average / 2).toFixed(1)}/10 IMDb
+				{vote_average.toFixed(1)}/10 IMDb
 			</Text>
 		</View>
 	)
